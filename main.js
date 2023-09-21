cd818be33419706818285ed8559f79009409e910
Andrezimdopvp <102673694+Andrezimdopvp@users.noreply.github.com>
2023-09-21 19:25:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 som="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload()
 {
     som=loadSound("Me.mp3");
@@ -9,6 +13,13 @@ function setup()
     caixa.center();
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log('PoseNet Is Initializeted');
 }
 function draw()
 {
@@ -17,4 +28,19 @@ function draw()
 function iniciar()
 {
     som.play();
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" lefyWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+    }
+    
 }
