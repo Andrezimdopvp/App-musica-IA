c08c6c168d6e4f91083bda9040e8b80b831ad26a
Andrezimdopvp <102673694+Andrezimdopvp@users.noreply.github.com>
2023-09-30 17:31:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -37,27 +37,22 @@ if(scoreRightWrist > 0.2)
 
     if(rightWristY >0 && rightWristY <= 100)
     {
-        document.getElementById("speed").innerHTML = "Velocidade = 0.5px";
         som.rate(0.5);
     }
     else if(rightWristY >100 && rightWristY <= 200)
     {
-        document.getElementById("speed").innerHTML = "Velocidade = 1px";
         som.rate(1);
     }
     else if(rightWristY >200 && rightWristY <= 300)
     {
-        document.getElementById("speed").innerHTML = "Velocidade = 1.5px";
         som.rate(1.5);
     }
     else if(rightWristY >300 && rightWristY <= 400)
     {
-        document.getElementById("speed").innerHTML = "Velocidade = 2px";
         som.rate(2);
     }
     else if(rightWristY >400)
     {
-        document.getElementById("speed").innerHTML = "Velocidade = 2.5px";
         som.rate(2.5);
     }
 }
@@ -67,7 +62,6 @@ circle(leftWristX, leftWristY,20);
 InNumberleftWristY = Number(leftWristY);
 remove_decimals = floor(InNumberleftWristY);
 volume = remove_decimals/500;
-document.getElementById("volume").innerHTML = "Volume = " + volume;
 som.setVolume(volume);
 }
 }
